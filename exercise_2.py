44a98ffa078f918f8102dfe1107824076942b34b
Cássio Cioni Carmo <cassiocioni@hotmail.com>
2023-03-30 18:52:16 -0300

    Create exercise_2.py

---
+++ exercise_2.py
@@ -0,0 +1,12 @@
+"""
+    Exercice - 2: The sum of two numbers
+    A python program just to add two numbers passed by the user and show the result
+    
+    Cássio Cioni Carmo - 03/30/2023
+"""
+
+if __name__ == "__main__":
+    first_num = int(input("Insert the first number:"))
+    second_num = int(input("Insert the second number:"))
+    
+    print("The sum of the numbers is:", first_num + second_num)
