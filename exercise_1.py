12297a60582177fd381e60f3fcaa55d774f490b0
Cássio Cioni Carmo <cassiocioni@hotmail.com>
2023-03-30 18:43:50 -0300

    Create exercise_1.py

---
+++ exercise_1.py
@@ -0,0 +1,9 @@
+"""
+    Exercice - 1: Hello World!
+    First python program just to print the phrase "Hello World!"
+    
+    Cássio Cioni Carmo - 03/30/2023
+"""
+
+if __name__ == "__main__":
+    print("Hello World!")
