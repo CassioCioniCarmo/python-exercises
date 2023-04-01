e6db96e9bc62a44bb5fe4a0755bdbd98f984bf3c
Cássio Cioni Carmo <cassiocioni@hotmail.com>
2023-04-01 15:34:59 -0300

    Create exercise_6.py

---
+++ exercise_6.py
@@ -0,0 +1,16 @@
+"""
+   Exercice - 6: Print Function
+    
+   Sample Input 0
+   3
+   
+   Sample Output 0
+   123
+    
+    Cassio Cioni Carmo - 04/01/2023
+"""
+
+if __name__ == '__main__':
+    number = int(input("Insert the number:"))
+    for i in range (1,number +1):
+        print(i, end='')
