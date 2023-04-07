6477db7b69896d9893656ebc707217ea77bdcf65
Cássio Cioni Carmo <cassiocioni@hotmail.com>
2023-04-06 21:39:55 -0300

    Added exercice 8

---
+++ exercise_8.py
@@ -0,0 +1,43 @@
+"""
+   Exercice - 8:  Print a list of all possible coordinates given by (i, j, k)  on a 3D grid where the sum of i + j + k is not equal to n.
+   Print the list in lexicographic increasing order.
+
+   Sample Input 0
+    1
+    1
+    1
+    2
+
+    Sample Output 0
+    [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+
+    Sample Input 1
+    2
+    2
+    2
+    2
+
+    Sample Output 1
+    [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 2], [0, 2, 1], [0, 2, 2], 
+    [1, 0, 0], [1, 0, 2], [1, 1, 1], [1, 1, 2], [1, 2, 0], [1, 2, 1], 
+    [1, 2, 2], [2, 0, 1], [2, 0, 2], [2, 1, 0], [2, 1, 1], [2, 1, 2], 
+    [2, 2, 0], [2, 2, 1], [2, 2, 2]]
+    
+    Cassio Cioni Carmo - 04/06/2023
+"""
+
+if __name__ == '__main__':
+    numerber_x = int(input())
+    numerber_y = int(input())
+    numerber_z = int(input())
+    numerber_n = int(input())
+    
+    lst=[]
+    
+    for i in range(numerber_x + 1):
+        for j in range(numerber_y + 1):
+            for k in range(numerber_z + 1):
+                if i+j+k != numerber_n:
+                    lst.append(f"[{i}, {j}, {k}]")
+
+    print("["+", ".join(lst)+"]") 
