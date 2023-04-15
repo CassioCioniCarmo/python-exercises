19bfb8aae7859184b0237af0ceb91c83fd74d0fa
Cássio Cioni Carmo <cassiocioni@hotmail.com>
2023-04-15 19:18:25 -0300

    Added exercise 15

---
+++ exercise_15.py
@@ -0,0 +1,26 @@
+"""
+    Exercise - 14:  Find a string
+
+   Sample Input
+    ABCDCDC
+    CDC
+
+    Sample Output
+    2
+        
+    Cassio Cioni Carmo - 04/15/2023
+"""
+def count_substring(string, sub_string):
+    total_sub_string = 0
+    for i in range(len(string)):
+        if string[i: i +len(sub_string)] == sub_string:
+            total_sub_string += 1
+    
+    return total_sub_string
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
